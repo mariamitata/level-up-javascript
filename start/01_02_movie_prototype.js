97478d3a18c5df8d3c3ae7358106dcaa009a9627
mariamitata <makotatanashvili@gmail.com>
2023-07-24 09:43:47 +0000

    Movie Prototype with class solution

--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,1 +1,24 @@
 // Write your code here
+class Movie {
+  constructor(title, director, genre, release_year, rating) {
+  this.title = title;
+  this.director = director;
+  this.genre = genre;
+  this.release_year = release_year;
+  this.rating = rating;
+  }
+  //getter
+  get overview() {
+    return this.movidOverview();
+  }
+
+  //Method
+  movidOverview () {
+    console.log(`${this.title}, a ${this.genre} film directed by ${this.director} 
+    was released in ${this.release_year}. It received a rating of ${this.rating}`);
+  }
+}
+
+const LordOfTheRings = new Movie("The Lord of the Rings", "Peter Jackson", "Drama", 2003, 9.0 );
+
+console.log(LordOfTheRings.overview)
