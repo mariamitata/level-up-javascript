0734a7edfe975296ba030a124ea11d2a40fdb709
mariamitata <makotatanashvili@gmail.com>
2023-07-24 12:13:43 +0000

    private object with Symbols

--- start/01_05_private_object.js
+++ start/01_05_private_object.js
@@ -1,1 +1,12 @@
 // Write your code here
+const username = Symbol("username");
+const password = Symbol("password");
+
+const user = {
+  [username]: "MariamiTa",
+  [password]: "123456", 
+  age: 35
+}
+
+console.log(user.username);
+console.log(user.password);
